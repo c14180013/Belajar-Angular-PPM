3fdb3ce55e73ed257f7da4610dc328076804ce40
c14180013 <c14180013@john.petra.ac.id>
2020-04-15 08:53:46 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'My Name';
+
+  itembuah = [{
+    nama : 'Kelengkeng',
+    harga : 10000
+  }, {
+    nama : 'Jeruk',
+    harga : 5000
+  }];
+  
+  itemArr = ['aa', 'bb'];
+
+  show: boolean = true;
+
+  user = ""
+
+  pesan = ''
+
+  klikButton() {
+    this.pesan = "Tombol ditekan"
+  }
 }
